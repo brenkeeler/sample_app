f5e257670c3566f09d64e8524f999bd87859573c
Brendan Keeler <bkeeler@nd.edu>
2010-10-07 08:12:30 -0400

    Finished layout and route

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,8 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
-map.root :controller => 'pages', :action => 'home'
+  map.root :controller => 'pages', :action => 'home'
+  map.signup '/signup',   :controller => 'users', :action => 'new'
 
 
 
